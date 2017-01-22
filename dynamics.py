31a7568869158e8deec75b40d2b67275dada71d8
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2017-01-22 14:53:51 +0200

    training works

--- dynamics.py
+++ dynamics.py
@@ -46,7 +46,7 @@ def run(time_out=3., epochs=1):
 		Q = rl.init_Q()
 
 	for ep in range(epochs): 
-		if ep%100 == 0: print "epoch:",ep
+		if ep%1000 == 0: print "episodes:",ep
 
 		## initial conditions
 		x = 0.
@@ -54,25 +54,30 @@ def run(time_out=3., epochs=1):
 		a = random.gauss(0., 0.1)
 		da = 0.
 		v_target = random.choice([0.,1.,2.,3.])
-		
+
 		t = 0.
 		log = []
 		state = rl.get_state(dx,a,da,v_target)
+		#print 'state:',state
 			
 		while t < time_out:
 			t += tau
 
 			u,val = rl.get_policy_val(state,Q)
+			#print 'u,val:',u,val
 			x,dx,a,da = get_next_values(x,dx,a,da,u,tau)
 			next_state = rl.get_state(dx,a,da,v_target)
-			reward = rl.get_reward(state,next_state,u)
-			rl.learn(state,u,next_state,Q)
-			state = next_state
-
-			log.append((t,dx,a,u))
+			if abs(a) > 1.1: 
+				reward = -100.
+				rl.learn(state,u,next_state,reward,Q)
+				break
+			else:
+				reward = rl.get_reward(state,next_state,u)
+				rl.learn(state,u,next_state,reward,Q)
+				state = next_state
+				log.append((t,dx,a,u))
 			
-			if abs(a) > 0.7: break
-
+	print 'target velosity:', v_target
 	show(log)
 	save_q_tab(Q)
 
@@ -140,10 +145,10 @@ if __name__ == '__main__':
 	elif len(args) == 3 and args[1] == 'run': 
 		t = float(args[2])
 		run(time_out=t)
-	elif len(args) == 2 and args[1] == 'train': train()
+	elif len(args) == 2 and args[1] == 'train': run(epochs=100)
 	elif len(args) == 3 and args[1] == 'train': 
 		N = int(args[2])
-		train(epochs=N)
+		run(epochs=N)
 	else:
 		print "\nUSAGE:\n\tpython dynamics.py run [time] OR\n" +\
 				"python dynamics train [N]\n"
--- rl.py
+++ rl.py
@@ -7,6 +7,7 @@ import numpy as np
 
 EPS = 0.2
 L_RATE = 0.7
+GAMMA = 0.97
 
 U_MAX = 6
 
@@ -40,18 +41,41 @@ def state2index((dx,a,da,v)):
 def get_policy_val(state,Q):
 	ind = state2index(state)
 		
-	if random.random() < EPS: u = random.choice(range(-U_MAX, U_MAX+1))
-	else: u = np.argmax(Q[ind])
-	
+	probs = softmax(Q[ind])
+	#print probs
+	u = weighted_choice(probs)
+
 	if u > U_MAX: u = u - U_MAX*2 - 1
-	return ( Q[ind][u], u )
+	#print 'control:',u
+	return ( u,Q[ind][u] )
 
 
 
-def get_reward(state,next_state,u): return 0.
+def get_reward(state,next_state,u):
+	(dx,_,_,v) = next_state
+	return 10 - abs(u) - abs(dx-v)
 
 
 
-def learn(state,u,next_state,Q): pass
+def learn(state,u,next_state,reward,Q): 
+	ind = state2index(state)
+	ind1 = state2index(next_state)
+	Val = np.max(Q[ind1])
+	Q[ind][u] = (1 - L_RATE)*Q[ind][u] + L_RATE*(reward + GAMMA*Val)
+	
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+
+def weighted_choice(ps):
+	s = 0.
+	for i in range(len(ps)):
+		s += ps[i]
+		if random.random() < s: return i
+	return len(ps)
+
 
 
